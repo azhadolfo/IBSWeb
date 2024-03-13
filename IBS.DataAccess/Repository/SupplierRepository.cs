c9b357c883629103639b25896865bbab55702bf1
azh <azhadolfo1@gmail.com>
2024-03-13 18:44:40 +0800

    create crud for Supplier

--- IBS.DataAccess/Repository/SupplierRepository.cs
+++ IBS.DataAccess/Repository/SupplierRepository.cs
@@ -1,6 +1,8 @@
 ﻿using IBS.DataAccess.Data;
 using IBS.DataAccess.Repository.IRepository;
 using IBS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,84 @@ namespace IBS.DataAccess.Repository
         public SupplierRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
+        }
+
+        public async Task<string> GenerateCodeAsync(CancellationToken cancellationToken = default)
+        {
+            Supplier? lastSupplier = await _db
+                .Suppliers
+                .OrderBy(s => s.SupplierId)
+                .LastOrDefaultAsync(cancellationToken);
+
+            if (lastSupplier != null)
+            {
+                string lastCode = lastSupplier.SupplierCode;
+                string numericPart = lastCode.Substring(1);
+
+                // Parse the numeric part and increment it by one
+                int incrementedNumber = int.Parse(numericPart) + 1;
+
+                // Format the incremented number with leading zeros and concatenate with the letter part
+                return $"{lastCode[0]}{incrementedNumber:D6}"; //e.g S000002
+            }
+            else
+            {
+                return "S000001";
+            }
+        }
+
+        public async Task<bool> IsSupplierExistAsync(string supplierName, CancellationToken cancellationToken = default)
+        {
+            return await _db.Suppliers
+                .AnyAsync(s => s.SupplierName == supplierName, cancellationToken);
+        }
+
+        public async Task<bool> IsTinNoExistAsync(string tin, CancellationToken cancellationToken = default)
+        {
+            return await _db.Suppliers
+                .AnyAsync(s => s.SupplierTin == tin, cancellationToken);
+        }
+
+        public async Task<string> SaveProofOfRegistration(IFormFile file, string localPath, CancellationToken cancellationToken = default)
+        {
+            if (!Directory.Exists(localPath))
+            {
+                Directory.CreateDirectory(localPath);
+            }
+
+            string fileName = Path.GetFileName(file.FileName);
+            string fileSavePath = Path.Combine(localPath, fileName);
+
+            await using (FileStream stream = new(fileSavePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream, cancellationToken);
+            }
+
+            return fileSavePath;
+        }
+
+        public async Task UpdateAsync(Supplier model, CancellationToken cancellationToken = default)
+        {
+            Supplier existingSupplier = await _db.Suppliers
+                .FindAsync(model.SupplierId, cancellationToken) ?? throw new InvalidOperationException($"Supplier with id '{model.SupplierId}' not found.");
+
+            existingSupplier.SupplierName = model.SupplierName;
+            existingSupplier.SupplierAddress = model.SupplierAddress;
+            existingSupplier.SupplierTin = model.SupplierTin;
+            existingSupplier.SupplierTerms = model.SupplierTerms;
+            existingSupplier.VatType = model.VatType;
+            existingSupplier.TaxType = model.TaxType;
+
+            if (_db.ChangeTracker.HasChanges())
+            {
+                existingSupplier.EditedBy = "Ako";
+                existingSupplier.EditedDate = DateTime.Now;
+                await _db.SaveChangesAsync(cancellationToken);
+            }
+            else
+            {
+                throw new InvalidOperationException("No data changes!");
+            }
         }
     }
 }
--- IBS.Utility/SD.cs
+++ IBS.Utility/SD.cs
@@ -8,15 +8,19 @@ namespace IBS.Utility
 {
     public static class SD
     {
-        public const string VatType_Vatable = "Vatable";
-        public const string VatType_ZeroRated = "Zero-Rated";
-        public const string VatType_Exempt = "Exempt";
-
         public const string Terms_Cod = "COD";
         public const string Terms_7d = "7D";
         public const string Terms_15d = "15D";
         public const string Terms_30d = "30D";
         public const string Terms_M15 = "M15";
         public const string Terms_M30 = "M30";
+
+        public const string VatType_Vatable = "Vatable";
+        public const string VatType_ZeroRated = "Zero-Rated";
+        public const string VatType_Exempt = "Exempt";
+
+        public const string TaxType_WithTax = "Withholding Tax";
+        public const string TaxType_WithVax = "Withholding Vat";
+        public const string TaxType_Exempt = "Exempt";
     }
 }
