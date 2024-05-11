b91f170dde9f516014f959576b6e1700cfa43324
azh <azhadolfo1@gmail.com>
2024-05-11 17:02:47 +0800

    added feature: capturing the offline transaction

---
+++ IBS.DataAccess/Migrations/20240511040742_CreateOfflineTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOfflineTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offlines",
+                columns: table => new
+                {
+                    OfflineId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StationCode = table.Column<string>(type: "varchar(3)", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Product = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Pump = table.Column<int>(type: "integer", nullable: false),
+                    Opening = table.Column<double>(type: "double precision", nullable: false),
+                    Closing = table.Column<double>(type: "double precision", nullable: false),
+                    Liters = table.Column<double>(type: "double precision", nullable: false),
+                    Balance = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offlines", x => x.OfflineId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offlines");
+        }
+    }
+}
---
+++ IBS.Models/Offline.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IBS.Models
+{
+    public class Offline
+    {
+        [Key]
+        public int OfflineId { get; set; }
+
+        [Column(TypeName = "varchar(3)")]
+        public string StationCode { get; set; } //fuel.StationCode
+
+        [Column(TypeName = "date")]
+        public DateOnly StartDate { get; set; } //fuel.BusinessDate - previous
+
+        [Column(TypeName = "date")]
+        public DateOnly EndDate { get; set; } //fuel.BusinessDate
+
+        [Column(TypeName = "varchar(20)")]
+        public string Product { get; set; } //fuel product
+
+        public int Pump { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N3}", ApplyFormatInEditMode = true)]
+        [Column(TypeName = "double()")]
+        public double Opening { get; set; } //fuel.Opening - previous
+
+        [DisplayFormat(DataFormatString = "{0:N3}", ApplyFormatInEditMode = true)]
+        public double Closing { get; set; } //fuel.Closing
+
+        [DisplayFormat(DataFormatString = "{0:N3}", ApplyFormatInEditMode = true)]
+        public double Liters { get; set; } //Closing - Opening
+
+        [DisplayFormat(DataFormatString = "{0:N3}", ApplyFormatInEditMode = true)]
+        public double Balance { get; set; } //Remaining Balance
+
+        public Offline(string stationCode, DateOnly startDate, DateOnly endDate, string product, int pump, double opening, double closing)
+        {
+            StationCode = stationCode;
+            StartDate = startDate;
+            EndDate = endDate;
+            Product = product;
+            Pump = pump;
+            Opening = opening;
+            Closing = closing;
+            Liters = opening - closing;
+            Balance = Liters;
+        }
+
+    }
+}
