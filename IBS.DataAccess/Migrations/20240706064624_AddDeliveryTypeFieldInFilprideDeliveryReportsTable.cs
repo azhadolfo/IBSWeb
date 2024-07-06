302cc200510b94d829c8cad82e51457df0fdbec7
azh <azhadolfo1@gmail.com>
2024-07-06 15:41:16 +0800

    migration for implementing the selection of for pick up and direct delivery

---
+++ IBS.DataAccess/Migrations/20240706064624_AddDeliveryTypeFieldInFilprideDeliveryReportsTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryTypeFieldInFilprideDeliveryReportsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "delivery_type",
+                table: "filpride_delivery_receipts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "delivery_type",
+                table: "filpride_delivery_receipts",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
