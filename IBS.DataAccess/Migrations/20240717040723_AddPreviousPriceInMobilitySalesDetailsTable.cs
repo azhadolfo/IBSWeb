c680b8bc9ea8c13625cdf68cc47553f93e50e4a6
azh <azhadolfo1@gmail.com>
2024-07-17 13:02:27 +0800

    migration in the previous commit

---
+++ IBS.DataAccess/Migrations/20240717040723_AddPreviousPriceInMobilitySalesDetailsTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviousPriceInMobilitySalesDetailsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "previous_price",
+                table: "mobility_sales_details",
+                type: "numeric(18,4)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "previous_price",
+                table: "mobility_sales_details");
+        }
+    }
+}
