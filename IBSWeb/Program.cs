9da389c6d92ec5e99d5fcaf659cd77b1646afd15
azhadolfo <azhadolfo1@gmail.com>
2024-11-15 17:57:08 +0800

    fix: error encountered on production

--- IBSWeb/Program.cs
+++ IBSWeb/Program.cs
@@ -45,7 +45,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("User/Home/Error");
+    app.UseExceptionHandler("/User/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
