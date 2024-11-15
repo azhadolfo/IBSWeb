5578461f0cfd0ccfb9803551acefb57013e84fa8
azhadolfo <azhadolfo1@gmail.com>
2024-11-15 15:16:58 +0800

    feat: add maintenance page and environment to cut the request of user

---
+++ IBS.DataAccess/Migrations/20241115065609_CreateNewtableAppSettings.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewtableAppSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "app_settings",
+                columns: table => new
+                {
+                    setting_key = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_app_settings", x => x.setting_key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_app_settings_setting_key",
+                table: "app_settings",
+                column: "setting_key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "app_settings");
+        }
+    }
+}
