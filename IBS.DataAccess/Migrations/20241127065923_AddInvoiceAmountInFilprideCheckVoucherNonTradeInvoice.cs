1dc80caf09600e85332b4652ddfee0d82463b7ed
azhadolfo <azhadolfo1@gmail.com>
2024-11-27 15:27:43 +0800

    BREAKING CHANGE: major revision on creation cv non trade invoice

---
+++ IBS.DataAccess/Migrations/20241127065923_AddInvoiceAmountInFilprideCheckVoucherNonTradeInvoice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoiceAmountInFilprideCheckVoucherNonTradeInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "invoice_amount",
+                table: "filpride_check_voucher_headers",
+                type: "numeric(18,4)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "invoice_amount",
+                table: "filpride_check_voucher_headers");
+        }
+    }
+}
