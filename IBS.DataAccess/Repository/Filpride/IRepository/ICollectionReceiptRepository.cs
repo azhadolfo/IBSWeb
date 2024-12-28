062be32b9f196c850f1fff340db0bc7734bfa7cf
crisleo14 <andulanleonard@gmail.com>
2024-12-28 16:58:38 +0800

    fix: mapping if documented or undoc when selecting service invoice in creating collection receipt

--- IBS.DataAccess/Repository/Filpride/IRepository/ICollectionReceiptRepository.cs
+++ IBS.DataAccess/Repository/Filpride/IRepository/ICollectionReceiptRepository.cs
@@ -6,7 +6,7 @@ namespace IBS.DataAccess.Repository.Filpride.IRepository
 {
     public interface ICollectionReceiptRepository : IRepository<FilprideCollectionReceipt>
     {
-        Task<string> GenerateCodeAsync(string company, CancellationToken cancellationToken = default);
+        Task<string> GenerateCodeAsync(string company, string type, CancellationToken cancellationToken = default);
 
         Task<string> GenerateCodeForSIAsync(string company, string type, CancellationToken cancellationToken = default);
 
