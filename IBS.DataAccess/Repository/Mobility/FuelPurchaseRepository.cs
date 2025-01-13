c1be8deb08b9f7746d00e8d2015f591823fc4ffa
danSambajon <sambajondanaron@gmail.com>
2025-01-13 19:04:51 +0800

    revision of the acct title mobility function

--- IBS.DataAccess/Repository/Mobility/FuelPurchaseRepository.cs
+++ IBS.DataAccess/Repository/Mobility/FuelPurchaseRepository.cs
@@ -90,7 +90,7 @@ namespace IBS.DataAccess.Repository.Mobility
 
                 List<MobilityGeneralLedger> journals = new();
 
-                var (inventoryAcctNo, inventoryAcctTitle) = GetInventoryAccountTitle(product.ProductCode);
+                var (inventoryAcctNo, inventoryAcctTitle) = MobilityGetInventoryAccountTitle(product.ProductCode);
 
                 journals.Add(new MobilityGeneralLedger
                 {
