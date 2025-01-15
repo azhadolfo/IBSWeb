1846d97ccfdbb496840d28bfa2ac757d1514e252
azhadolfo <azhadolfo1@gmail.com>
2025-01-15 11:26:57 +0800

    feat: user able to change the validity date

--- IBSWeb/Areas/Filpride/Controllers/AuthorityToLoadController.cs
+++ IBSWeb/Areas/Filpride/Controllers/AuthorityToLoadController.cs
@@ -254,5 +254,34 @@ namespace IBSWeb.Areas.Filpride.Controllers
 
             return Json(haulerDetails);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateValidityDate(int id, DateOnly newValidUntil, CancellationToken cancellationToken)
+        {
+            var companyClaims = await GetCompanyClaimAsync();
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            try
+            {
+                var existingAtl = await _unitOfWork.FilprideAuthorityToLoad
+                    .GetAsync(atl => atl.AuthorityToLoadId == id, cancellationToken);
+
+                existingAtl.ValidUntil = newValidUntil;
+
+                FilprideAuditTrail auditTrailBook = new(existingAtl.CreatedBy, $"Update validity date of atl# {existingAtl.AuthorityToLoadNo}", "Authority To Load", "", companyClaims);
+                await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
+
+                await _unitOfWork.SaveAsync(cancellationToken);
+                await transaction.CommitAsync(cancellationToken);
+
+                return Json(new { success = true });
+
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                return Json(new { success = false });
+            }
+        }
     }
 }
