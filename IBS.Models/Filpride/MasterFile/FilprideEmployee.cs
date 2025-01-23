7ee7e685662f75a787bfbdea5084d0424d1884d4
azhadolfo <azhadolfo1@gmail.com>
2025-01-23 12:41:45 +0800

    feat: add filpride employee table

---
+++ IBS.Models/Filpride/MasterFile/FilprideEmployee.cs
@@ -0,0 +1,82 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IBS.Models.Filpride.MasterFile
+{
+    public class FilprideEmployee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        public string EmployeeNumber { get; set; }
+
+        public string Initial { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(100)")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(100)")]
+        public string MiddleName { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(100)")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(5)")]
+        public string Suffix { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(255)")]
+        public string Address { get; set; } = string.Empty;
+
+        [Column(TypeName = "date")]
+        public DateOnly BirthDate { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string TelNo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(20)")]
+        public string SssNo { get; set; } = string.Empty;
+
+        [RegularExpression(@"\d{3}-\d{3}-\d{3}-\d{5}", ErrorMessage = "Invalid TIN number format.")]
+        [Column(TypeName = "varchar(20)")]
+        public string TinNo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(20)")]
+        public string PhilhealthNo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(20)")]
+        public string PagibigNo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(20)")]
+        public string Company { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar(20)")]
+        public string Depatment { get; set; } = string.Empty;
+
+        [Column(TypeName = "date")]
+        public DateOnly DateHired { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateOnly? DateResigned { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string Position { get; set; }
+
+        public bool IsManagerial { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string Supervisor { get; set; }
+
+        [Column(TypeName = "varchar(5)")]
+        public string Status { get; set; }
+
+        public string Paygrade { get; set; } = string.Empty;
+
+        [Column(TypeName = "numeric(18,2)")]
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
