0ad6b1ad16fe5099d84b78a2f0de7ad6d00f11df
danSambajon <sambajondanaron@gmail.com>
2025-03-12 16:30:30 +0800

    feat: create and edit pickup points

---
+++ IBSWeb/Areas/Filpride/Views/PickupPoint/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBSWeb.Areas.Filpride.Views.PickupPoint
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
