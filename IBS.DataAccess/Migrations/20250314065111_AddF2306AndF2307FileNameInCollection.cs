a6d84e5a39b2466118d1842091bf43f75c020706
azhadolfo <azhadolfo1@gmail.com>
2025-03-14 15:07:28 +0800

    feat: make the uploading of file in to cloud

---
+++ IBS.DataAccess/Migrations/20250314065111_AddF2306AndF2307FileNameInCollection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddF2306AndF2307FileNameInCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "f2306file_name",
+                table: "filpride_collection_receipts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "f2307file_name",
+                table: "filpride_collection_receipts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "f2306file_name",
+                table: "filpride_collection_receipts");
+
+            migrationBuilder.DropColumn(
+                name: "f2307file_name",
+                table: "filpride_collection_receipts");
+        }
+    }
+}
