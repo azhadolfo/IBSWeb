3373502f84c930d018fe61c08fbe23c85d380332
azhadolfo <azhadolfo1@gmail.com>
2025-04-26 14:39:24 +0800

    feat: implement the importing of po sales and deposit (migration)

---
+++ IBS.DataAccess/Migrations/20250426060650_AddIsProcessedInMobilityFmsPoSales.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsProcessedInMobilityFmsPoSales : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_processed",
+                table: "mobility_fms_po_sales",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_processed",
+                table: "mobility_fms_po_sales");
+        }
+    }
+}
