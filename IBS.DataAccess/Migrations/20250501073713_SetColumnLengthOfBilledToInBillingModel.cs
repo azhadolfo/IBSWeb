688d4479dd9b04012af950347afba45ba3f32128
crisleo14 <andulanleonard@gmail.com>
2025-05-01 15:40:18 +0800

    fix: change value added tax into billed to local and principal

---
+++ IBS.DataAccess/Migrations/20250501073713_SetColumnLengthOfBilledToInBillingModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetColumnLengthOfBilledToInBillingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "billed_to",
+                table: "mmsi_billings",
+                type: "varchar(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "billed_to",
+                table: "mmsi_billings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)");
+        }
+    }
+}
