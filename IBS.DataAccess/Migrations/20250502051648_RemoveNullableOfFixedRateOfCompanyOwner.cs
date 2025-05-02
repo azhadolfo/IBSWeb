365270a4a44eeb3e55c0374777ded7daf9b7402a
danSambajon <sambajondanaron@gmail.com>
2025-05-02 13:25:33 +0800

    fix: removed fixed rate nullability of tug provider

---
+++ IBS.DataAccess/Migrations/20250502051648_RemoveNullableOfFixedRateOfCompanyOwner.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNullableOfFixedRateOfCompanyOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "fixed_rate",
+                table: "mmsi_company_owners",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "fixed_rate",
+                table: "mmsi_company_owners",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
