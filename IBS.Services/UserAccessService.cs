8f7f8c4bae16d70770e87291274bd83e81b79283
danSambajon <sambajondanaron@gmail.com>
2025-05-10 12:03:20 +0800

    feat: creation of user access service

---
+++ IBS.Services/UserAccessService.cs
@@ -0,0 +1,56 @@
+using IBS.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace IBS.Services
+{
+    public interface IUserAccessService
+    {
+        Task<bool> CheckAccess(IdentityUser? user, string moduleName, CancellationToken cancellationToken = default);
+    }
+
+    public class UserAccessService : IUserAccessService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserAccessService(UserManager<IdentityUser> userManager, ApplicationDbContext dbContext)
+        {
+            _userManager = userManager;
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> CheckAccess(IdentityUser? user, string moduleName, CancellationToken cancellationToken = default)
+        {
+            var userAccess = await _dbContext.MMSIUserAccesses?
+                .FirstOrDefaultAsync(a => a.UserId == user.Id, cancellationToken);
+
+            if (userAccess == null)
+            {
+                return false;
+            }
+
+            switch (moduleName)
+            {
+                case "Create service request":
+                    return userAccess.CanCreateServiceRequest;
+                case "Post service request":
+                    return userAccess.CanPostServiceRequest;
+                case "Create dispatch ticket":
+                    return userAccess.CanCreateDispatchTicket;
+                case "Set tariff":
+                    return userAccess.CanSetTariff;
+                case "Approve tariff":
+                    return userAccess.CanApproveTariff;
+                case "Create billing":
+                    return userAccess.CanCreateBilling;
+                case "Create collection":
+                    return userAccess.CanCreateCollection;
+                default:
+                    return false;
+            }
+        }
+    }
+}
