dcc2587b087704dd5dda0dfb6b087921eb664c85
danSambajon <sambajondanaron@gmail.com>
2025-05-24 13:42:40 +0800

    fix: add loggers to catch of service controller

--- IBSWeb/Areas/MMSI/Controllers/ServiceController.cs
+++ IBSWeb/Areas/MMSI/Controllers/ServiceController.cs
@@ -11,10 +11,12 @@ namespace IBSWeb.Areas.MMSI.Controllers
     public class ServiceController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly ILogger<ServiceController> _logger;
 
-        public ServiceController(ApplicationDbContext db)
+        public ServiceController(ApplicationDbContext db, ILogger<ServiceController> logger)
         {
             _db = db;
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -52,14 +54,8 @@ namespace IBSWeb.Areas.MMSI.Controllers
 
             catch (Exception ex)
             {
-                if (!string.IsNullOrEmpty(ex.InnerException?.Message))
-                {
-                    TempData["error"] = ex.InnerException.Message;
-                }
-                else
-                {
-                    TempData["error"] = ex.Message;
-                }
+                _logger.LogError(ex, "Failed to create service.");
+                TempData["error"] = ex.Message;
 
                 return View(model);
             }
@@ -84,8 +80,11 @@ namespace IBSWeb.Areas.MMSI.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "Failed to delete service.");
+                TempData["error"] = ex.Message;
+
                 return RedirectToAction(nameof(Index));
             }
         }
@@ -121,8 +120,8 @@ namespace IBSWeb.Areas.MMSI.Controllers
 
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
-                TempData["error"] = "An error occurred while editing the entry. Please try again.";
+                _logger.LogError(ex, "Failed to edit service.");
+                TempData["error"] = ex.Message;
 
                 return View(model);
             }
