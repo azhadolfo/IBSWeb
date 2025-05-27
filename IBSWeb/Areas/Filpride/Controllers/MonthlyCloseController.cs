a37016cf250c2a278493196f9a55ed7a35782bed
azhadolfo <azhadolfo1@gmail.com>
2025-05-27 16:37:24 +0800

    feat: convert the monthly closing to manual

---
+++ IBSWeb/Areas/Filpride/Controllers/MonthlyCloseController.cs
@@ -0,0 +1,52 @@
+using IBS.Services;
+using IBS.Services.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Quartz;
+
+namespace IBSWeb.Areas.Filpride.Controllers
+{
+    [Area(nameof(Filpride))]
+    [Authorize(Roles = "Admin")]
+    public class MonthlyCloseController : Controller
+    {
+        private readonly ISchedulerFactory _schedulerFactory;
+
+        public MonthlyCloseController(ISchedulerFactory schedulerFactory)
+        {
+            _schedulerFactory = schedulerFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TriggerMonthlyClosure(DateTime monthDate)
+        {
+            try
+            {
+                var scheduler = await _schedulerFactory.GetScheduler();
+
+                var dataMap = new JobDataMap
+                {
+                    { "monthDate", monthDate },
+                };
+
+                var jobKey = JobKey.Create(nameof(MonthlyClosureService));
+
+                await scheduler.TriggerJob(jobKey, dataMap);
+
+                TempData["success"] = $"Month of {monthDate:MMM yyyy} closed successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+    }
+}
