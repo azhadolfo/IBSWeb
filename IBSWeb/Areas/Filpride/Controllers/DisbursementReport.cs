849b4141534cb37722e6278cfe1223d9f1e47204
crisleo14 <andulanleonard@gmail.com>
2025-05-28 14:41:26 +0800

    feat: implement report viewer in purchase order report module

--- IBSWeb/Areas/Filpride/Controllers/DisbursementReport.cs
+++ IBSWeb/Areas/Filpride/Controllers/DisbursementReport.cs
@@ -13,6 +13,8 @@ using IBS.Utility.Helpers;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace IBSWeb.Areas.Filpride.Controllers
 {
@@ -206,5 +208,148 @@ namespace IBSWeb.Areas.Filpride.Controllers
                 return RedirectToAction(nameof(ClearedDisbursementReport));
             }
         }
+
+        #region -- Generate Cleared Disbursement Report Excel File --
+
+        public async Task<IActionResult> GenerateClearedDisbursementReportExcelFile(ViewModelBook model, CancellationToken cancellationToken)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Please input date range";
+                return RedirectToAction(nameof(ClearedDisbursementReport));
+            }
+
+            try
+            {
+                var dateFrom = model.DateFrom;
+                var dateTo = model.DateTo;
+                var extractedBy = _userManager.GetUserName(User)!;
+                var companyClaims = await GetCompanyClaimAsync();
+                if (companyClaims == null)
+                {
+                    return BadRequest();
+                }
+
+                var clearedDisbursementReport =
+                    await _unitOfWork.FilprideReport.GetClearedDisbursementReport(model.DateFrom, model.DateTo,
+                        companyClaims);
+
+
+                if (clearedDisbursementReport.Count == 0)
+                {
+                    TempData["error"] = "No Record Found";
+                    return RedirectToAction(nameof(clearedDisbursementReport));
+                }
+
+                // Create the Excel package
+                using var package = new ExcelPackage();
+                // Add a new worksheet to the Excel package
+                var worksheet = package.Workbook.Worksheets.Add("ClearedDisbursementReport");
+
+                // Set the column headers
+                var mergedCells = worksheet.Cells["A1:C1"];
+                mergedCells.Merge = true;
+                mergedCells.Value = "CLEARED DISBURSEMENT REPORT";
+                mergedCells.Style.Font.Size = 13;
+
+                worksheet.Cells["A2"].Value = "Date Range:";
+                worksheet.Cells["A3"].Value = "Extracted By:";
+                worksheet.Cells["A4"].Value = "Company:";
+
+                worksheet.Cells["B2"].Value = $"{dateFrom} - {dateTo}";
+                worksheet.Cells["B3"].Value = $"{extractedBy}";
+                worksheet.Cells["B4"].Value = $"{companyClaims}";
+
+                worksheet.Cells["A7"].Value = "Category";
+                worksheet.Cells["B7"].Value = "Subcategory";
+                worksheet.Cells["C7"].Value = "Payee";
+                worksheet.Cells["D7"].Value = "Date";
+                worksheet.Cells["E7"].Value = "Voucher #";
+                worksheet.Cells["F7"].Value = "Bank Name";
+                worksheet.Cells["G7"].Value = "Check #";
+                worksheet.Cells["H7"].Value = "Particulars";
+                worksheet.Cells["I7"].Value = "Amount";
+
+                // Apply styling to the header row
+                using (var range = worksheet.Cells["A7:I7"])
+                {
+                    range.Style.Font.Bold = true;
+                    range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    range.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                    range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                }
+
+                // Populate the data rows
+                int row = 8;
+                string currencyFormat = "#,##0.00";
+
+                foreach (var cd in clearedDisbursementReport)
+                {
+
+                    var details = await _dbContext.FilprideCheckVoucherDetails
+                        .Where(d => d.CheckVoucherHeaderId == cd.CheckVoucherHeaderId)
+                        .ToListAsync(cancellationToken);
+
+                    var invoiceDebit = details
+                        .FirstOrDefault(d => d.Debit > 0);
+
+                    var getCategoryInChartOfAccount = await _dbContext.FilprideChartOfAccounts
+                        .Include(coa => coa.ParentAccount) // Level 3
+                        .ThenInclude(a => a!.ParentAccount) // Level 2
+                        .ThenInclude(a => a!.ParentAccount) // Level 1
+                        .Where(coa => coa.AccountNumber == invoiceDebit!.AccountNo)
+                        .FirstOrDefaultAsync(cancellationToken);
+
+                    var levelOneAccount = getCategoryInChartOfAccount?.ParentAccount?.ParentAccount;
+
+                    worksheet.Cells[row, 1].Value = $"{levelOneAccount?.AccountNumber} " +
+                                                    $"{levelOneAccount?.AccountName}";
+                    worksheet.Cells[row, 2].Value = $"{invoiceDebit!.AccountNo} {invoiceDebit.AccountName}";
+                    worksheet.Cells[row, 3].Value = cd.Payee;
+                    worksheet.Cells[row, 4].Value = cd.Date;
+                    worksheet.Cells[row, 5].Value = cd.CheckVoucherHeaderNo;
+                    worksheet.Cells[row, 6].Value = cd.BankAccount!.AccountName;
+                    worksheet.Cells[row, 7].Value = cd.CheckNo;
+                    worksheet.Cells[row, 8].Value = cd.Particulars;
+                    worksheet.Cells[row, 9].Value = cd.Total;
+
+                    worksheet.Cells[row, 4].Style.Numberformat.Format = "MMM/dd/yyyy";
+                    worksheet.Cells[row, 9].Style.Numberformat.Format = currencyFormat;
+
+                    row++;
+                }
+
+                worksheet.Cells[row, 8].Value = "Total: ";
+                worksheet.Cells[row, 9].Value = clearedDisbursementReport.Sum(cv => cv.Total);
+                using (var range = worksheet.Cells[row, 1, row, 9])
+                {
+                    range.Style.Border.Top.Style = ExcelBorderStyle.Thick; // Apply thick border at the top of the row
+                }
+
+                worksheet.Cells[row, 8].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                worksheet.Cells[row, 9].Style.Numberformat.Format = currencyFormat;
+                // Auto-fit columns for better readability
+                worksheet.Cells.AutoFitColumns();
+                worksheet.View.FreezePanes(8, 1);
+
+                // Convert the Excel package to a byte array
+                var excelBytes = package.GetAsByteArray();
+
+                return File(excelBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    $"ClearedDisbursementReport_{DateTime.UtcNow.AddHours(8):yyyyddMMHHmmss}.xlsx");
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return RedirectToAction(nameof(ClearedDisbursementReport));
+            }
+        }
+
+
+        #endregion
     }
 }
