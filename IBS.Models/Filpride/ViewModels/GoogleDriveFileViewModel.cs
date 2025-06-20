f6cf63c9d72fba9d49a17d7a09d22c2c5f9ef6ee
danSambajon <sambajondanaron@gmail.com>
2025-06-20 18:37:02 +0800

    feat: uploading of cv details to gdrive

---
+++ IBS.Models/Filpride/ViewModels/GoogleDriveFileViewModel.cs
@@ -0,0 +1,9 @@
+namespace IBS.Models.Filpride.ViewModels
+{
+    public class GoogleDriveFileViewModel
+    {
+        public string FileName { get; set; }
+        public string FileLink { get; set; }
+        public byte[] FileContent { get; set; }
+    }
+}
