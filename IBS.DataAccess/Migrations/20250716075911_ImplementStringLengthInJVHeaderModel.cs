2c191574831c2626f2770bb1ee537e06f9bb387a
crisleo14 <andulanleonard@gmail.com>
2025-07-16 16:04:44 +0800

    feat: implement string length in journal voucher header model

---
+++ IBS.DataAccess/Migrations/20250716075911_ImplementStringLengthInJVHeaderModel.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementStringLengthInJVHeaderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "filpride_journal_voucher_headers",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "filpride_journal_voucher_headers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "journal_voucher_header_no",
+                table: "filpride_journal_voucher_headers",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "company",
+                table: "filpride_journal_voucher_headers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "filpride_journal_voucher_headers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "filpride_journal_voucher_headers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "journal_voucher_header_no",
+                table: "filpride_journal_voucher_headers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "company",
+                table: "filpride_journal_voucher_headers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
