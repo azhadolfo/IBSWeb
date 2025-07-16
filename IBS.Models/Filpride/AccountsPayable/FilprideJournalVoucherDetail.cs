b36af471f46bde20949e244f20b20c651f841f40
crisleo14 <andulanleonard@gmail.com>
2025-07-16 15:53:28 +0800

    faet: implement string length in journal voucher details model

--- IBS.Models/Filpride/AccountsPayable/FilprideJournalVoucherDetail.cs
+++ IBS.Models/Filpride/AccountsPayable/FilprideJournalVoucherDetail.cs
@@ -9,16 +9,20 @@ namespace IBS.Models.Filpride.AccountsPayable
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int JournalVoucherDetailId { get; set; }
 
+        [StringLength(20)]
         public string AccountNo { get; set; } = " ";
+
+        [StringLength(200)]
         public string AccountName { get; set; } = " ";
 
+        [StringLength(13)]
         public string TransactionNo { get; set; } = " ";
 
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal Debit { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal Credit { get; set; }
 
