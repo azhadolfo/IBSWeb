f38f48570cb5226519fe084891131f9d8ade69a3
crisleo14 <andulanleonard@gmail.com>
2025-07-17 15:12:51 +0800

    feat: implement string length in customer branch model

---
+++ IBS.DataAccess/Migrations/20250717070830_ImplementStringLengthInCustomerBranchModel.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementStringLengthInCustomerBranchModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_tin",
+                table: "filpride_customer_branches",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_name",
+                table: "filpride_customer_branches",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_address",
+                table: "filpride_customer_branches",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_tin",
+                table: "filpride_customer_branches",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_name",
+                table: "filpride_customer_branches",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "branch_address",
+                table: "filpride_customer_branches",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
