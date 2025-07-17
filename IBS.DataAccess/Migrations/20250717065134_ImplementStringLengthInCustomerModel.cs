0d1e37c70a48bb82240b968860f7409bbb13eaef
crisleo14 <andulanleonard@gmail.com>
2025-07-17 15:04:21 +0800

    feat: implement string length in customer model

---
+++ IBS.DataAccess/Migrations/20250717065134_ImplementStringLengthInCustomerModel.cs
@@ -0,0 +1,280 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementStringLengthInCustomerModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "zip_code",
+                table: "filpride_customers",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "vat_type",
+                table: "filpride_customers",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "filpride_customers",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "station_code",
+                table: "filpride_customers",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "edited_by",
+                table: "filpride_customers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_type",
+                table: "filpride_customers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_tin",
+                table: "filpride_customers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_terms",
+                table: "filpride_customers",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_name",
+                table: "filpride_customers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_code",
+                table: "filpride_customers",
+                type: "character varying(7)",
+                maxLength: 7,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(7)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_address",
+                table: "filpride_customers",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "created_by",
+                table: "filpride_customers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "company",
+                table: "filpride_customers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "business_style",
+                table: "filpride_customers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "zip_code",
+                table: "filpride_customers",
+                type: "varchar(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "vat_type",
+                table: "filpride_customers",
+                type: "varchar(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "filpride_customers",
+                type: "varchar(15)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "station_code",
+                table: "filpride_customers",
+                type: "varchar(3)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(3)",
+                oldMaxLength: 3,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "edited_by",
+                table: "filpride_customers",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_type",
+                table: "filpride_customers",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_tin",
+                table: "filpride_customers",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_terms",
+                table: "filpride_customers",
+                type: "varchar(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_name",
+                table: "filpride_customers",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_code",
+                table: "filpride_customers",
+                type: "varchar(7)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(7)",
+                oldMaxLength: 7,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "customer_address",
+                table: "filpride_customers",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "created_by",
+                table: "filpride_customers",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "company",
+                table: "filpride_customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "business_style",
+                table: "filpride_customers",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
