9f95615ac4f30d0b97d812b25e9710d7f4ad5ef4
crisleo14 <andulanleonard@gmail.com>
2025-07-17 16:05:38 +0800

    feat: implement string length in supplier model

--- IBS.Models/Filpride/MasterFile/FilprideSupplier.cs
+++ IBS.Models/Filpride/MasterFile/FilprideSupplier.cs
@@ -12,81 +12,86 @@ namespace IBS.Models.Filpride.MasterFile
         public int SupplierId { get; set; }
 
         [Display(Name = "Supplier Code")]
-        [Column(TypeName = "varchar(7)")]
+        [StringLength(7)]
         public string? SupplierCode { get; set; }
 
         [Display(Name = "Supplier Name")]
-        [Column(TypeName = "varchar(200)")]
+        [StringLength(200)]
         public string SupplierName { get; set; }
 
         [Display(Name = "Supplier Address")]
-        [Column(TypeName = "varchar(200)")]
+        [StringLength(200)]
         public string SupplierAddress { get; set; }
 
-        [Column(TypeName = "varchar(20)")]
+        [StringLength(20)]
         [RegularExpression(@"\d{3}-\d{3}-\d{3}-\d{5}", ErrorMessage = "Invalid TIN number format.")]
         [Display(Name = "Tin No")]
         public string SupplierTin { get; set; }
 
-        [Column(TypeName = "varchar(10)")]
+        [StringLength(10)]
         [Display(Name = "Supplier Terms")]
         public string SupplierTerms { get; set; }
 
-        [Column(TypeName = "varchar(10)")]
+        [StringLength(10)]
         [Display(Name = "VAT Type")]
         public string VatType { get; set; }
 
-        [Column(TypeName = "varchar(20)")]
+        [StringLength(20)]
         [Display(Name = "TAX Type")]
         public string TaxType { get; set; }
 
-        [Column(TypeName = "varchar(1024)")]
+        [StringLength(1024)]
         public string? ProofOfRegistrationFilePath { get; set; }
 
+        [StringLength(200)]
         public string? ProofOfRegistrationFileName { get; set; }
 
-        [Column(TypeName = "varchar(1024)")]
+        [StringLength(1024)]
         public string? ProofOfExemptionFilePath { get; set; }
 
+        [StringLength(200)]
         public string? ProofOfExemptionFileName { get; set; }
 
         public bool IsActive { get; set; } = true;
 
         [Display(Name = "Created By")]
-        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
         public string? CreatedBy { get; set; }
 
         [Display(Name = "Created Date")]
+        [Column(TypeName = "timestamp with time zone")]
         public DateTime CreatedDate { get; set; } = DateTimeHelper.GetCurrentPhilippineTime();
 
         [Display(Name = "Edited By")]
-        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
         public string? EditedBy { get; set; }
 
         [Display(Name = "Edited Date")]
+        [Column(TypeName = "timestamp with time zone")]
         public DateTime? EditedDate { get; set; }
 
-        [Column(TypeName = "varchar(20)")]
+        [StringLength(20)]
         public string Category { get; set; }
 
-        [Column(TypeName = "varchar(255)")]
+        [StringLength(255)]
         [Display(Name = "Trade Name")]
         public string? TradeName { get; set; }
 
-        [Column(TypeName = "varchar(20)")]
+        [StringLength(20)]
         public string? Branch { get; set; }
 
-        [Column(TypeName = "varchar(100)")]
+        [StringLength(100)]
         [Display(Name = "Default Expense")]
         public string? DefaultExpenseNumber { get; set; }
 
         [NotMapped]
         public List<SelectListItem>? DefaultExpenses { get; set; }
 
+        [Column(TypeName = "numeric(18,4)")]
         [Display(Name = "Withholding Tax Percent")]
         public decimal? WithholdingTaxPercent { get; set; }
 
-        [Column(TypeName = "varchar(100)")]
+        [StringLength(100)]
         [Display(Name = "Withholding Tax Title")]
         public string? WithholdingTaxtitle { get; set; }
 
@@ -94,21 +99,22 @@ namespace IBS.Models.Filpride.MasterFile
         public List<SelectListItem>? WithholdingTaxList { get; set; }
 
         [Display(Name = "Reason")]
-        [Column(TypeName = "varchar(100)")]
+        [StringLength(100)]
         public string? ReasonOfExemption { get; set; }
 
-        [Column(TypeName = "varchar(20)")]
+        [StringLength(20)]
         public string? Validity { get; set; }
 
         [Display(Name = "Validity Date")]
-        [Column(TypeName = "date")]
+        [Column(TypeName = "timestamp with time zone")]
         public DateTime? ValidityDate { get; set; }
 
+        [StringLength(20)]
         public string Company { get; set; } = string.Empty;
 
         [Required]
         [Display(Name = "Zip Code")]
-        [Column(TypeName = "varchar(10)")]
+        [StringLength(10)]
         public string? ZipCode { get; set; }
 
         public bool IsFilpride { get; set; }
