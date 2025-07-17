116da6938ae6b0ac7a4ab5c2f2b54c38da7ff07c
crisleo14 <andulanleonard@gmail.com>
2025-07-17 10:23:39 +0800

    feat: implement string length in credit memo model

--- IBS.Models/Filpride/AccountsReceivable/FilprideCreditMemo.cs
+++ IBS.Models/Filpride/AccountsReceivable/FilprideCreditMemo.cs
@@ -10,7 +10,7 @@ namespace IBS.Models.Filpride.AccountsReceivable
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CreditMemoId { get; set; }
 
-        [Column(TypeName = "varchar(12)")]
+        [StringLength(13)]
         [Display(Name = "CM No")]
         public string? CreditMemoNo { get; set; }
 
@@ -47,20 +47,21 @@ namespace IBS.Models.Filpride.AccountsReceivable
         private string _description;
 
         [Display(Name = "Price Adjustment")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal? AdjustedPrice { get; set; }
 
         [Column(TypeName = "numeric(18,4)")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         public decimal? Quantity { get; set; }
 
         [Display(Name = "Credit Amount")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal CreditAmount { get; set; }
 
         [Required]
+        [StringLength(20)]
         public string Source { get; set; }
 
         [StringLength(1000)]
@@ -75,24 +76,27 @@ namespace IBS.Models.Filpride.AccountsReceivable
         [Column(TypeName = "date")]
         public DateOnly Period { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal? Amount { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal CurrentAndPreviousAmount { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.0000;(#,##0.0000)}", ApplyFormatInEditMode = false)]
         [Column(TypeName = "numeric(18,4)")]
         public decimal UnearnedAmount { get; set; }
 
+        [StringLength(20)]
         public string Company { get; set; } = string.Empty;
 
         public bool IsPrinted { get; set; }
 
+        [StringLength(50)]
         public string Status { get; set; } = nameof(Utility.Enums.Status.Pending);
 
+        [StringLength(13)]
         public string? Type { get; set; }
     }
 }
