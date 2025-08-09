7b0ef0192121b23a0113dcc49a920ef181226d3c
danSambajon <sambajondanaron@gmail.com>
2025-08-09 11:08:29 +0800

    fix: enhance mobility masterfile - customer

--- IBSWeb/Areas/Mobility/Controllers/CustomerController.cs
+++ IBSWeb/Areas/Mobility/Controllers/CustomerController.cs
@@ -69,8 +69,7 @@ namespace IBSWeb.Areas.Mobility.Controllers
                 return NotFound();
             }
 
-            var customer = await _unitOfWork
-                .MobilityCustomer
+            var customer = await _unitOfWork.MobilityCustomer
                 .GetAsync(c => c.CustomerId == id, cancellationToken);
 
             if (customer == null)
@@ -78,19 +77,32 @@ namespace IBSWeb.Areas.Mobility.Controllers
                 return NotFound();
             }
 
-            customer.IsActive = true;
-            await _unitOfWork.SaveAsync(cancellationToken);
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
 
-            #region --Audit Trail Recording
+            try
+            {
+                customer.IsActive = true;
+                await _unitOfWork.SaveAsync(cancellationToken);
 
-            FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
-                $"Activated customer {customer.CustomerCode}", "Customer", nameof(Mobility));
-            await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
+                #region --Audit Trail Recording
 
-            #endregion -- Audit Trail Recording
+                FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
+                    $"Activated customer {customer.CustomerCode}", "Customer", nameof(Mobility));
+                await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
 
-            TempData["success"] = "Customer has been activated";
-            return RedirectToAction(nameof(Index));
+                #endregion -- Audit Trail Recording
+
+                await transaction.CommitAsync(cancellationToken);
+                TempData["success"] = "Customer has been activated";
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Failed to activate customer master file. Created by: {UserName}", _userManager.GetUserName(User));
+                await transaction.RollbackAsync(cancellationToken);
+                TempData["error"] = ex.Message;
+                return RedirectToAction(nameof(Activate), new { id = id });
+            }
         }
 
         public async Task<IActionResult> Index(CancellationToken cancellationToken)
@@ -119,57 +131,58 @@ namespace IBSWeb.Areas.Mobility.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(MobilityCustomer model, CancellationToken cancellationToken)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
-
-                try
-                {
-                    var stationCodeClaims = await GetStationCodeClaimAsync();
-
-                    if (stationCodeClaims == null)
-                    {
-                        return NotFound();
-                    }
-
-                    //bool IsTinExist = await _unitOfWork.FilprideCustomer.IsTinNoExistAsync(model.CustomerTin, companyClaims, cancellationToken);
-                    bool IsTinExist = false;
-                    if (!IsTinExist)
-                    {
-                        model.StationCode = stationCodeClaims;
-                        model.CustomerCode = await _unitOfWork.MobilityCustomer.GenerateCodeAsync(model.CustomerType, stationCodeClaims, cancellationToken);
-                        model.CreatedBy = _userManager.GetUserName(User);
-                        await _unitOfWork.MobilityCustomer.AddAsync(model, cancellationToken);
-                        await _unitOfWork.SaveAsync(cancellationToken);
-
-                        #region --Audit Trail Recording
-
-                        FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
-                            $"Create new customer {model.CustomerCode}", "Customer", nameof(Mobility));
-                        await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
-
-                        #endregion --Audit Trail Recording
-
-                        ViewData["StationCode"] = stationCodeClaims;
-
-                        await transaction.CommitAsync(cancellationToken);
-                        TempData["success"] = "Customer created successfully";
-                        return RedirectToAction(nameof(Index));
-                    }
-
-                    ModelState.AddModelError("CustomerTin", "Tin No already exist.");
-                    return View(model);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Failed to create customer master file. Created by: {UserName}", _userManager.GetUserName(User));
-                    await transaction.RollbackAsync(cancellationToken);
-                    TempData["error"] = ex.Message;
-                    return View(model);
-                }
+                ModelState.AddModelError("", "Make sure to fill all the required details.");
+                return View(model);
             }
-            ModelState.AddModelError("", "Make sure to fill all the required details.");
-            return View(model);
+
+            //bool IsTinExist = await _unitOfWork.FilprideCustomer.IsTinNoExistAsync(model.CustomerTin, companyClaims, cancellationToken);
+            bool IsTinExist = false;
+
+            if (IsTinExist)
+            {
+                ModelState.AddModelError("CustomerTin", "Tin No already exist.");
+                return View(model);
+            }
+
+            var stationCodeClaims = await GetStationCodeClaimAsync();
+
+            if (stationCodeClaims == null)
+            {
+                return NotFound();
+            }
+
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            try
+            {
+                model.StationCode = stationCodeClaims;
+                model.CustomerCode = await _unitOfWork.MobilityCustomer.GenerateCodeAsync(model.CustomerType, stationCodeClaims, cancellationToken);
+                model.CreatedBy = _userManager.GetUserName(User);
+                await _unitOfWork.MobilityCustomer.AddAsync(model, cancellationToken);
+                await _unitOfWork.SaveAsync(cancellationToken);
+                ViewData["StationCode"] = stationCodeClaims;
+
+                #region --Audit Trail Recording
+
+                FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
+                    $"Create new customer {model.CustomerCode}", "Customer", nameof(Mobility));
+                await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
+
+                #endregion --Audit Trail Recording
+
+                await transaction.CommitAsync(cancellationToken);
+                TempData["success"] = "Customer created successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to create customer master file. Created by: {UserName}", _userManager.GetUserName(User));
+                await transaction.RollbackAsync(cancellationToken);
+                TempData["error"] = ex.Message;
+                return View(model);
+            }
         }
 
         [HttpGet]
@@ -200,8 +213,7 @@ namespace IBSWeb.Areas.Mobility.Controllers
                 return NotFound();
             }
 
-            var customer = await _unitOfWork
-                .MobilityCustomer
+            var customer = await _unitOfWork.MobilityCustomer
                 .GetAsync(c => c.CustomerId == id, cancellationToken);
 
             if (customer == null)
@@ -209,20 +221,32 @@ namespace IBSWeb.Areas.Mobility.Controllers
                 return NotFound();
             }
 
-            customer.IsActive = false;
-            await _unitOfWork.SaveAsync(cancellationToken);
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
 
-            #region --Audit Trail Recording
+            try
+            {
+                customer.IsActive = false;
+                await _unitOfWork.SaveAsync(cancellationToken);
 
-            FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
-                $"Deactivated customer {customer.CustomerCode}", "Customer", nameof(Mobility));
-            await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
+                #region --Audit Trail Recording
 
-            #endregion -- Audit Trail Recording
+                FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
+                    $"Deactivated customer {customer.CustomerCode}", "Customer", nameof(Mobility));
+                await _unitOfWork.FilprideAuditTrail.AddAsync(auditTrailBook, cancellationToken);
 
-            TempData["success"] = "Customer has been deactivated";
-            return RedirectToAction(nameof(Index));
+                #endregion -- Audit Trail Recording
 
+                await transaction.CommitAsync(cancellationToken);
+                TempData["success"] = "Customer has been deactivated";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to create customer master file. Created by: {UserName}", _userManager.GetUserName(User));
+                await transaction.RollbackAsync(cancellationToken);
+                TempData["error"] = ex.Message;
+                return RedirectToAction(nameof(Deactivate), new { id = id });
+            }
         }
 
         [HttpGet]
@@ -251,63 +275,61 @@ namespace IBSWeb.Areas.Mobility.Controllers
         [HttpPost]
         public async Task<IActionResult> Edit(MobilityCustomer model, CancellationToken cancellationToken)
         {
-            var stationCodeClaims = await GetStationCodeClaimAsync();
-            ViewData["StationCode"] = stationCodeClaims;
-            if (ModelState.IsValid)
-            {
-                await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
-
-                try
-                {
-                    #region -- getMobilityStation --
-
-                    var getMobilityStation = await _dbContext.MobilityStations
-                                                .Where(s => s.StationCode == stationCodeClaims)
-                                                .FirstOrDefaultAsync(cancellationToken);
-
-                    #endregion -- getMobilityStation --
-
-                    var existingCustomer = await _unitOfWork.MobilityCustomer.GetAsync(c => c.CustomerId == model.CustomerId, cancellationToken);
-
-                    if (existingCustomer == null)
-                    {
-                        return NotFound();
-                    }
-
-                    #region -- Assign New Values --
-
-                    await _unitOfWork.MobilityCustomer.UpdateAsync(model, cancellationToken);
-
-                    #endregion -- Assign New Values --
-
-                    await _dbContext.SaveChangesAsync(cancellationToken);
-                    TempData["success"] = "Customer updated successfully";
-
-                    #region --Audit Trail Recording
-
-                    FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
-                        $"Edited customer {existingCustomer.CustomerCode} => {model.CustomerCode}", "Customer", nameof(Mobility));
-                    await _dbContext.FilprideAuditTrails.AddAsync(auditTrailBook, cancellationToken);
-
-                    #endregion -- Audit Trail Recording
-
-                    await transaction.CommitAsync(cancellationToken);
-
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (Exception ex)
-                {
-                    await transaction.RollbackAsync(cancellationToken);
-                    _logger.LogError(ex, "Failed to edit customer master file. Created by: {UserName}", _userManager.GetUserName(User));
-                    model.MobilityStations = await _unitOfWork.GetMobilityStationListAsyncByCode(cancellationToken);
-                    TempData["error"] = $"Error: '{ex.Message}'";
-                    return View(model);
-                }
-            }
-            else
+            if (!ModelState.IsValid)
             {
                 model.MobilityStations = await _unitOfWork.GetMobilityStationListAsyncByCode(cancellationToken);
                 ModelState.AddModelError("", "The information you submitted is not valid!");
+                return View(model);
+            }
+
+            var existingCustomer = await _unitOfWork.MobilityCustomer
+                .GetAsync(c => c.CustomerId == model.CustomerId, cancellationToken);
+
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            try
+            {
+                var stationCodeClaims = await GetStationCodeClaimAsync();
+                ViewData["StationCode"] = stationCodeClaims;
+
+                #region -- getMobilityStation --
+
+                var getMobilityStation = await _dbContext.MobilityStations
+                    .Where(s => s.StationCode == stationCodeClaims)
+                    .FirstOrDefaultAsync(cancellationToken);
+
+                #endregion -- getMobilityStation --
+
+                #region -- Assign New Values --
+
+                await _unitOfWork.MobilityCustomer.UpdateAsync(model, cancellationToken);
+                await _unitOfWork.SaveAsync(cancellationToken);
+
+                #endregion -- Assign New Values --
+
+                #region --Audit Trail Recording
+
+                FilprideAuditTrail auditTrailBook = new(_userManager.GetUserName(User)!,
+                    $"Edited customer {existingCustomer.CustomerCode} => {model.CustomerCode}", "Customer", nameof(Mobility));
+                await _dbContext.FilprideAuditTrails.AddAsync(auditTrailBook, cancellationToken);
+
+                #endregion -- Audit Trail Recording
+
+                await transaction.CommitAsync(cancellationToken);
+                TempData["success"] = "Customer updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to edit customer master file. Created by: {UserName}", _userManager.GetUserName(User));
+                await transaction.RollbackAsync(cancellationToken);
+                model.MobilityStations = await _unitOfWork.GetMobilityStationListAsyncByCode(cancellationToken);
+                TempData["error"] = $"Error: '{ex.Message}'";
                 return View(model);
             }
         }
