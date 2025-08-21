0d20d123989afedab26a7fd02aeac0dcb7bb1b16
Dan Sambajon <sambajondanaron@gmail.com>
2025-08-21 12:06:33 +0800

    feat: addition of checkBank to cr model

--- IBS.DataAccess/Migrations/20250821040545_AddedCheckBankToCrModel.Designer.cs
+++ IBS.DataAccess/Migrations/20250821040545_AddedCheckBankToCrModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IBS.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IBS.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250821040545_AddedCheckBankToCrModel")]
+    partial class AddedCheckBankToCrModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1645,8 +1648,8 @@ namespace IBS.DataAccess.Migrations
 
                     b.Property<string>("CollectionReceiptNo")
                         .IsRequired()
-                        .HasMaxLength(13)
-                        .HasColumnType("character varying(13)")
+                        .HasMaxLength(15)
+                        .HasColumnType("character varying(15)")
                         .HasColumnName("collection_receipt_no");
 
                     b.Property<DateOnly>("InvoiceDate")
