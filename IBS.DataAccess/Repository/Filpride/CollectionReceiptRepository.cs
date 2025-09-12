e45186af42ffca15ad6ec7c6c480c3684e3c02fb
danSambajon <sambajondanaron@gmail.com>
2025-09-12 16:22:29 +0800

    fix: bug in update multiple invoice in collection receipt

--- IBS.DataAccess/Repository/Filpride/CollectionReceiptRepository.cs
+++ IBS.DataAccess/Repository/Filpride/CollectionReceiptRepository.cs
@@ -651,6 +651,7 @@ namespace IBS.DataAccess.Repository.Filpride
             {
                 var siValue = siNo[i];
                 var salesInvoice = await _db.FilprideSalesInvoices
+                                       .Where(si => si.Status != "Voided")
                     .FirstOrDefaultAsync(p => p.SalesInvoiceNo == siValue, cancellationToken)
                                    ?? throw new NullReferenceException("SalesInvoice not found");
 
