ab04d58718587ddc63dc0d090ec534a11c59daf4
crisleo14 <andulanleonard@gmail.com>
2025-11-27 11:51:10 +0800

    feat: additional filter on debit memo index

--- IBSWeb/Areas/Filpride/Controllers/DebitMemoController.cs
+++ IBSWeb/Areas/Filpride/Controllers/DebitMemoController.cs
@@ -74,7 +74,7 @@ namespace IBSWeb.Areas.Filpride.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> GetDebitMemos([FromForm] DataTablesParameters parameters, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetDebitMemos([FromForm] DataTablesParameters parameters, DateOnly filterDate, CancellationToken cancellationToken)
         {
             try
             {
@@ -99,6 +99,16 @@ namespace IBSWeb.Areas.Filpride.Controllers
                             s.Description.ToLower().Contains(searchValue) ||
                             s.CreatedBy!.ToLower().Contains(searchValue)
                             )
+                        .ToList();
+                }
+                if (filterDate != DateOnly.MinValue && filterDate != default)
+                {
+                    var searchValue = filterDate.ToString(SD.Date_Format).ToLower();
+
+                    debitMemos = debitMemos
+                        .Where(s =>
+                            s.TransactionDate.ToString(SD.Date_Format).ToLower().Contains(searchValue)
+                        )
                         .ToList();
                 }
 
