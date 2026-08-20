ba6e6faa097c5ffd0c0b023fe0710922e66614c0
crisleo14 <andulanleonard@gmail.com>
2026-08-20 13:49:20 +0800

    feat: add new fields cwt and cwvat in customer order slip module

--- IBSWeb/Areas/Filpride/Controllers/CustomerOrderSlipController.cs
+++ IBSWeb/Areas/Filpride/Controllers/CustomerOrderSlipController.cs
@@ -366,7 +366,9 @@ namespace IBSWeb.Areas.Filpride.Controllers
                     CommissioneeTaxType = commissionee?.TaxType,
                     BusinessStyle = customer.BusinessStyle,
                     AvailableCreditLimit = await _unitOfWork.FilprideCustomerOrderSlip
-                        .GetCustomerCreditBalance(customer.CustomerId, cancellationToken)
+                        .GetCustomerCreditBalance(customer.CustomerId, cancellationToken),
+                    CwtPercent = customer.CwtPercent,
+                    CwVatPercent = customer.CwVatPercent,
                 };
 
                 ///TODO Temporary solution for 14 days expiration of GASSO FUEL TRADING customer
